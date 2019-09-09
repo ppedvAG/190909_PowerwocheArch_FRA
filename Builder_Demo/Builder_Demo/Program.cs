de8d2533caa97d94713ddb2d93a5f879fccac4a7
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-09 11:20:25 +0200

    Builderpattern mit Schrank

--- Builder_Demo/Builder_Demo/Program.cs
+++ Builder_Demo/Builder_Demo/Program.cs
@@ -10,6 +10,23 @@ namespace Builder_Demo
     {
         static void Main(string[] args)
         {
+            var einfacherSchrank = Schrank.BaueSchrank()
+                                          .MitBöden(2)
+                                          .MitTüren(4)
+                                          .MitOberfläche(Schrank.Oberflächenart.Lackiert)
+                                          .InFarbe("Gelb")
+                                          .MitMetallschiene(true)
+                                          .Konstruiere();
+
+
+            var komplexer = Schrank.BaueSchrank()
+                                   .MitBöden(5)
+                                   .MitTüren(7)
+                                   .MitOberfläche(Schrank.Oberflächenart.Gewachst)
+                                   .MitMetallschiene(true)
+                                   .MitKleiderstange(true)
+                                   .Konstruiere();
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
---
+++ Builder_Demo/Builder_Demo/Schrank.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Demo
+{
+    class Schrank
+    {
+        private Schrank() { }
+
+        public int AnzahlTüren { get; set; }
+        public int AnzahlBöden { get; set; }
+        public string Farbe { get; set; }
+        public enum Oberflächenart { Unbehandelt, Gewachst,Lackiert}
+        public Oberflächenart Oberfläche { get; set; }
+        public bool Kleiderstange { get; set; }
+        public bool Metallschiene { get; set; }
+
+        public static SchrankBuilder BaueSchrank() => new SchrankBuilder();
+
+        public class SchrankBuilder // nested class
+        {
+            public SchrankBuilder()
+            {
+                zuBauenderSchrank = new Schrank();
+            }
+            private Schrank zuBauenderSchrank;
+
+            public SchrankBuilder MitBöden(int anzahl)
+            {
+                if (anzahl >= 0 && anzahl <= 6)
+                    zuBauenderSchrank.AnzahlBöden = anzahl;
+                else
+                    throw new ArgumentException("Die Anzahl der Böden ist ungültig");
+                return this;
+            }
+            public SchrankBuilder MitTüren(int anzahl)
+            {
+                if (anzahl >= 2 && anzahl <= 7)
+                    zuBauenderSchrank.AnzahlTüren = anzahl;
+                else
+                    throw new ArgumentException("Die Anzahl der Türen ist ungültig");
+                return this;
+            }
+            public SchrankBuilder MitOberfläche(Oberflächenart oberflächenart)
+            {
+                zuBauenderSchrank.Oberfläche = oberflächenart;
+                return this;
+            }
+            public SchrankBuilder InFarbe(string farbe)
+            {
+                if (zuBauenderSchrank.Oberfläche == Oberflächenart.Lackiert)
+                    zuBauenderSchrank.Farbe = farbe;
+                else
+                    throw new ArgumentException("Es darf nur ein lackierter Schrank eingefärbt werden !");
+                return this;
+            }
+            public SchrankBuilder MitMetallschiene(bool schiene)
+            {
+                zuBauenderSchrank.Metallschiene = schiene;
+                return this;
+            }
+            public SchrankBuilder MitKleiderstange(bool stange)
+            {
+                zuBauenderSchrank.Kleiderstange = stange;
+                return this;
+            }
+
+            public Schrank Konstruiere()
+            {
+                return zuBauenderSchrank;
+            }
+        }
+    }
+}
