7ad0d6ac35d84f6da27909a9db32742cade8cfb1
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-09 13:49:40 +0200

    Composite_Demo

---
+++ Composite_Demo/Composite_Demo/Aufgabe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Composite_Demo
+{
+    abstract class Aufgabe
+    {
+        public string Beschreibung { get; set; }
+        public virtual bool IstErledigt { get; set; }
+
+        public void AufgabeErledigen()
+        {
+            Thread.Sleep(5000);
+            IstErledigt = true;
+        }
+    }
+
+    class Einzelaufgabe : Aufgabe
+    {
+        public DateTime Deadline { get; set; }
+    }
+
+    class AufgabenListe : Aufgabe
+    {
+        public List<Aufgabe> Liste { get; set; } = new List<Aufgabe>();
+
+        public override bool IstErledigt // True wenn alle in der Liste bei "IstErledigt" true sind
+        {
+            get => Liste.All(x => x.IstErledigt == true);
+            set
+            {
+                foreach (var item in Liste)
+                {
+                    item.IstErledigt = value;
+                }
+            }
+        }
+    }
+
+
+}
--- Composite_Demo/Composite_Demo/Program.cs
+++ Composite_Demo/Composite_Demo/Program.cs
@@ -10,6 +10,22 @@ namespace Composite_Demo
     {
         static void Main(string[] args)
         {
+            AufgabenListe meineAufgaben = new AufgabenListe();
+            meineAufgaben.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 1",IstErledigt=true });
+            meineAufgaben.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 2", IstErledigt = true });
+            meineAufgaben.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 3", IstErledigt = true });
+            meineAufgaben.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 4", IstErledigt = true });
+
+            AufgabenListe mf2 = new AufgabenListe();
+            mf2.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 5", IstErledigt = true });
+            mf2.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 6", IstErledigt = true });
+            mf2.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 7", IstErledigt = true });
+            mf2.Liste.Add(new Einzelaufgabe { Beschreibung = "Demo 8", IstErledigt = true });
+
+            meineAufgaben.Liste.Add(mf2);
+
+            Console.WriteLine(meineAufgaben.SindAlleErledigt);
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
