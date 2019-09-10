778ee42e92afffff5391de39edfd3e0bebd359c3
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-10 10:14:01 +0200

    Interface für das Repository-Pattern erstellt

---
+++ MajoDDD/ppedv.MajoDDD.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.MajoDDD.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        void Add<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Update<T>(T item) where T : Entity;
+        T GetByID<T>(int ID) where T : Entity;
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        // Für LINQ:
+        IQueryable<T> Query<T>() where T : Entity;
+    }
+}
