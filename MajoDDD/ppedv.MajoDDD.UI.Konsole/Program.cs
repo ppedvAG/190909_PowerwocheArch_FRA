eeabdd55beef4891d4a72f5cf547439b173adc2e
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-10 11:39:57 +0200

    Testkonsole fertiggestellt

--- MajoDDD/ppedv.MajoDDD.UI.Konsole/Program.cs
+++ MajoDDD/ppedv.MajoDDD.UI.Konsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ppedv.MajoDDD.Data.EF;
+using ppedv.MajoDDD.Domain;
+using ppedv.MajoDDD.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,19 @@ namespace ppedv.MajoDDD.UI.Konsole
     {
         static void Main(string[] args)
         {
+            Core core = new Core(new EFRepository(new EFContext()));
+
+            if (core.Repository.GetAll<Book>().Count() == 0)
+                core.GenerateTestData();
+
+            foreach (var store in core.Repository.GetAll<BookStore>())
+            {
+                Console.WriteLine($"{store.Address}:");
+                foreach (var inv in store.InventoryList)
+                {
+                    Console.WriteLine($"\t{inv.Book.Title}: {inv.Amount} Stück");
+                }
+            }
 
 
             Console.WriteLine("---ENDE---");
