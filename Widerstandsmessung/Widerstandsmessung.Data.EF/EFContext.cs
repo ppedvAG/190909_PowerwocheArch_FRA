6f95efe607dce143b65552cd3b48b051f7233723
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-10 14:28:47 +0200

    Data-Layer erstellt

---
+++ Widerstandsmessung/Widerstandsmessung.Data.EF/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+using Widerstandsmessung.Domain;
+
+namespace Widerstandsmessung.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Server=(localdb)\\MSSQLLocalDB;Database=Widerstandsmessung;Trusted_Connection=true;AttachDbFilename=C:\\temp\\Widerstand.mdf")
+        {
+        }
+
+        public DbSet<SOLLWiderstand> SOLLWiderstand { get; set; }
+        public DbSet<ISTWiderstand> ISTWiderstand { get; set; }
+    }
+}
