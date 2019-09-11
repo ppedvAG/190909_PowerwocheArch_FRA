99ab4ac8ad69c0f26354edbf1b9790d637005e98
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-11 14:05:45 +0200

    OpenClosed - Prinzip mit ModularCalculator erklärt

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -13,7 +13,7 @@ namespace SOLID_Taschenrechner
         static void Main(string[] args)
         {
             var parser = new RegexParser();
-            var calculator = new IfCalculator();
+            var calculator = new ModularCalculator(new Addition(),new Subtraktion());
 
             new KonsolenUI(parser,calculator).Start();
         }
@@ -79,6 +79,43 @@ namespace SOLID_Taschenrechner
                     throw new FormatException($"Operator '{formel.Operator}' wird nicht unterstützt");
             }
         }
+        public class ModularCalculator : ICalculator
+        {
+            public ModularCalculator(params IRechenart[] rechenarten)
+            {
+                this.rechenarten = rechenarten;
+            }
+            private readonly IRechenart[] rechenarten;
+
+            public int Calculate(Formel f)
+            {
+                if (rechenarten.Any(x => x.Operator == f.Operator))
+                {
+                    var rechenart = rechenarten.First(x => x.Operator == f.Operator);
+                    return rechenart.Berechne(f.Operand1, f.Operand2);
+                }
+                else
+                    throw new FormatException($"Der Operator {f.Operator} wird nicht unterstüzt");
+            }
+        }
+
+
+        public interface IRechenart
+        {
+            string Operator { get; }
+            int Berechne(int z1, int z2);
+        }
+        public class Addition : IRechenart
+        {
+            public string Operator => "+";
+            public int Berechne(int z1, int z2) => z1 + z2;
+        }
+        public class Subtraktion : IRechenart
+        {
+            public string Operator => "-";
+            public int Berechne(int z1, int z2) => z1 - z2;
+        }
+
 
         public class KonsolenUI
         {
