a7dab914c1e99926eccc2a14c82fd543ba0279b1
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-11 15:48:01 +0200

    Neues Datenmodell für Widerstandsmessung

---
+++ Widerstandsmessung/Widerstandsmessung.Domain/DatenmodellNeu/Entity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Widerstandsmessung.Domain.DatenmodellNeu
+{
+    public abstract class Entity
+    {
+        public int ID { get; set; }
+    }
+
+    public class Spannung : Entity
+    {
+        public double Spannung1 { get; set; }
+        public double Spannung2 { get; set; }
+        public double Spannung3 { get; set; }
+    }
+    public class SollWiderstand : Entity
+    {
+        public double UV { get; set; }
+        public double UW { get; set; }
+        public double VW { get; set; }
+    }
+    public class IstWiderstand : Entity
+    {
+        public double UV { get; set; }
+        public double UW { get; set; }
+        public double VW { get; set; }
+        public double Mittelwert { get; set; }
+    }
+    public class SollWerte : Entity
+    {
+        public virtual SollWiderstand Widerstand { get; set; }
+        public virtual Spannung Spannungswerte { get; set; }
+    }
+    public class IstWerte : Entity
+    {
+        public virtual IstWiderstand Widerstand { get; set; }
+    }
+}
